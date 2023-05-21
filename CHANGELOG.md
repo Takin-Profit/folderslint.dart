ef525211e4e37d1671e723705ef44ecbbaab1216
g5becks <techstar.dev@hotmail.com>
2023-05-21 00:58:56 -0400

    fix: fix error in .gitignore parsing

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.2
+
+- improve logging
+
 ## 1.0.1
 
 - Minor changes to internals for speed.
--- lib/src/parse_git_ignore.dart
+++ lib/src/parse_git_ignore.dart
@@ -7,13 +7,16 @@ const String gitIgnore = '.gitignore';
 List<String> getIgnoredDirs() {
   try {
     final config = File(p.absolute('.gitignore')).readAsStringSync();
-    return config.split(lineSeparator()).map((line) => p.absolute(line)).toList();
+    return config
+        .split(lineSeparator())
+        .map((line) => p.absolute(line))
+        .where((line) => !line.startsWith('#') && line.isNotEmpty)
+        .toList();
+
   } catch (err) {
     stderr.writeln(err);
     exit(1);
   }
 }
 
-String lineSeparator() => Platform.isWindows
-      ? '\r\n'
-      : '\n';
+String lineSeparator() => Platform.isWindows ? '\r\n' : '\n';
