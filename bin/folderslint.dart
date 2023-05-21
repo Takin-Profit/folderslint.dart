97f6d54ad8da8c04fec6f671f461d77e2cc79b78
g5becks <techstar.dev@hotmail.com>
2023-05-21 00:42:55 -0400

    chore(docs): update version

--- bin/folderslint.dart
+++ bin/folderslint.dart
@@ -1,3 +1,5 @@
+import 'package:chalkdart/chalk.dart';
+import 'package:path/path.dart' as p;
 import 'package:folderslint/folderslint.dart';
 
 void main(List<String> arguments) {
@@ -5,6 +7,7 @@ void main(List<String> arguments) {
   final filePaths = getFileList(inputFiles: arguments);
 
   if (filePaths.isNotEmpty) {
+    print('running ${chalk.doubleUnderline(chalk.magenta(chalk.font7('folderslint')))} in ${chalk.cyan(p.current)}');
     runLinter(filePaths);
   }
 }
